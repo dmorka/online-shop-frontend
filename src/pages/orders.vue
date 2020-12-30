6c946ad3f380ba728fe41939437c0dde3ae1c729
dmorka <morkadawid@gmail.com>
2020-12-30 22:45:28 +0100

    created modal dialog

--- src/pages/orders.vue
+++ src/pages/orders.vue
@@ -2,14 +2,43 @@
   <b-container>
     <Navbar/>
     <h2 class="display-4 text-center">Orders</h2>
+    <button
+        type="button"
+        class="btn"
+        @click="showModal"
+    >
+      Open Modal!
+    </button>
+
+    <modal
+        v-show="isModalVisible"
+        @close="closeModal"
+    />
   </b-container>
 </template>
 
 <script>
 import Navbar from "@/components/navBar";
+import modal from "@/components/modal";
 export default {
   name: "orders",
-  components: {Navbar}
+  components: {
+    Navbar,
+    modal,
+  },
+  data () {
+    return {
+      isModalVisible: false,
+    };
+  },
+  methods: {
+    showModal() {
+      this.isModalVisible = true;
+    },
+    closeModal() {
+      this.isModalVisible = false;
+    }
+  },
 }
 </script>
 
